16513facba7856b4212f3886ac33eab34259bf16
Fabian Meyer <rookfighter@outlook.de>
2016-05-07 14:36:02 +0200

    Implement OrderedCrossover

---
+++ src/genetic/OrderedCrossover.cpp
@@ -0,0 +1,54 @@
+#include <cassert>
+#include <random>
+#include "OrderedCrossover.hpp"
+
+namespace tsp
+{
+    void OrderedCrossover::initGeneTracker(const unsigned int size)
+    {
+        individualGene_.resize(size);
+        populationGene_.resize(size);
+        for (unsigned int i = 0; i < individualGene_.size(); ++i)
+        {
+            individualGene_[i] = false;
+            populationGene_[i] = false;
+        }
+    }
+
+    void OrderedCrossover::cross(Individual &parent1, Individual &parent2, Individual &child)
+    {
+        assert(parent1.getPath().size() == parent2.getPath().size());
+
+        child.getPath().resize(parent1.getPath().size());
+        initGeneTracker(parent1.getPath().size());
+
+        unsigned int added = 0;
+        unsigned int toAdd = parent1.getPath().size() / 2;
+        while (added < toAdd)
+        {
+            int idx = std::rand() % parent1.getPath().size();
+            if (!individualGene_[idx])
+            {
+                ++added;
+                individualGene_[idx] = true;
+                populationGene_[parent1.getPath()[idx]] = true;
+                child.getPath()[idx] = parent1.getPath()[idx];
+            }
+        }
+
+        added = 0;
+        for (unsigned int i = 0; i < parent2.getPath().size(); ++i)
+        {
+            int node =  parent2.getPath()[i];
+            // check if this node was already used
+            if(populationGene_[node])
+                continue;
+
+            // find index to insert the gene
+            while(added < individualGene_.size() && individualGene_[added])
+                ++added;
+
+            child.getPath()[added] = node;
+        }
+    }
+}
---
+++ src/genetic/OrderedCrossover.hpp
@@ -0,0 +1,25 @@
+#ifndef GENETIC_ORDEREDCROSSOVER_HPP_
+#define GENETIC_ORDEREDCROSSOVER_HPP_
+
+#include "genetic/Individual.hpp"
+
+namespace tsp
+{
+    class OrderedCrossover
+    {
+    private:
+        std::vector<bool> individualGene_;
+        std::vector<bool> populationGene_;
+
+        void initGeneTracker(const unsigned int size);
+    public:
+        OrderedCrossover();
+        ~OrderedCrossover();
+
+        void cross(Individual &parent1, Individual &parent2, Individual &child);
+    };
+}
+
+
+
+#endif
