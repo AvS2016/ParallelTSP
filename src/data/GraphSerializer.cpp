3b523ee9283dfb592307809b8e627211af0ecaf1
JoramM <jorammarkert@gmx.de>
2016-05-10 10:57:59 +0200

    fix segmentation fault

--- src/data/GraphSerializer.cpp
+++ src/data/GraphSerializer.cpp
@@ -22,9 +22,9 @@ namespace tsp
         // deserialize nodes
         Json::Value &nodes = root["nodes"];
         graph.resize(nodes.size());
-        for(unsigned int i = 0; i < nodes.size(); ++i)
-            graph[i] = Node(nodes[i]["id"].asInt(), nodes[i]["x"].asInt(),
-                            nodes[i]["y"].asInt());
+        for(unsigned int i = 0; nodes.size(); ++i)
+            graph[i] = Node(nodes[i]["id"].asInt(), nodes[i]["x"].asInt(), nodes[i]["y"].asInt());
+
         return true;
     }
 
