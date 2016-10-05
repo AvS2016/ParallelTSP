e1e101a9ab926d984156ca68eef01013db22489c
TimoCode <TimoCode@github.com>
2016-10-05 15:29:52 +0200

    Fix Charts

--- src/GraphImageCreater/GraphImageCreater/GraphImage.Designer.cs
+++ src/GraphImageCreater/GraphImageCreater/GraphImage.Designer.cs
@@ -28,15 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-
-
 
             this.CrunchButton = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
@@ -55,21 +65,79 @@
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
             this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(0, 24);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series2.Legend = "Legend1";
-            series2.Name = "Linien";
-            this.chart1.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Linien";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(628, 342);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            this.chart1.Titles.Add(new System.Windows.Forms.DataVisualization.Charting.Title());
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea2";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend2";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(0, 24);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea2";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend2";
+            series2.Name = "Linien";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(628, 342);
+            this.chart2.TabIndex = 0;
+            this.chart2.Text = "chart2";
+            this.chart2.Titles.Add(new System.Windows.Forms.DataVisualization.Charting.Title());
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea3";
+            this.chart3.ChartAreas.Add(chartArea3);
+            this.chart3.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend3.Name = "Legend3";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(0, 34);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea3";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend3";
+            series3.Name = "Linien";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(638, 343);
+            this.chart3.TabIndex = 0;
+            this.chart3.Text = "chart3";
+            this.chart3.Titles.Add(new System.Windows.Forms.DataVisualization.Charting.Title());
+            // 
+            // chart4
+            // 
+            chartArea4.Name = "ChartArea4";
+            this.chart4.ChartAreas.Add(chartArea4);
+            this.chart4.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend4.Name = "Legend4";
+            this.chart4.Legends.Add(legend4);
+            this.chart4.Location = new System.Drawing.Point(0, 44);
+            this.chart4.Name = "chart4";
+            series4.ChartArea = "ChartArea4";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend4";
+            series4.Name = "Linien";
+            this.chart4.Series.Add(series4);
+            this.chart4.Size = new System.Drawing.Size(648, 444);
+            this.chart4.TabIndex = 0;
+            this.chart4.Text = "chart4";
+            this.chart4.Titles.Add(new System.Windows.Forms.DataVisualization.Charting.Title());
             // 
             // CrunchButton
             // 
